9b3cf74b0032e1c927aacfcc3e856bffb94a237f
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2024-01-24 14:09:11 +0200

    first part of the exersice

---
+++ ASP.NETIntoduction/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using ASP.NETIntoduction.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NETIntoduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home page";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Massage = "This is ASP.NET core MVC app";
+            return View();
+        }
+        public IActionResult Numbers()
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult NumbersToN(int n)
+        {
+           
+            return View("NumbersToN",n);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
