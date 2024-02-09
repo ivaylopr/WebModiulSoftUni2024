3f534013826d6f3efa77e53438444e355b08c2f3
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2024-02-09 17:34:45 +0200

    updating the proj

---
+++ TaskBoardApp/Data/DataConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskBoardApp.Data
+{
+	public static class DataConstants
+	{
+		public static class Task
+		{
+			public const int TitleMaxLenght = 70;
+			public const int TitleMinLenght = 5;
+			public const int DescriptionleMinLenght = 10;
+			public const int DesctiptionMaxLenght = 1000;
+			
+		}
+	}
+}
---
+++ TaskBoardApp/Data/Models/Task.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskBoardApp.Data.Models
+{
+    public class Task
+    {
+        [Key]
+        [Comment("Identifier")]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(DataConstants.Task.TitleMaxLenght)]
+        [Comment("Title of the task")]
+        public string Title { get; set; } = String.Empty;
+        [Required]
+        [MaxLength(DataConstants.Task.DesctiptionMaxLenght)]
+        [Comment("Description of the task")]
+        public string Description { get; set; } = String.Empty;
+        [Comment("Date when the task is created")]
+        public DateTime? CreatedOn { get; set; }
+        [Comment("Identifier of the board who is on the task")]
+        public int? BoardId { get; set; }
+        [Comment("Navigation prop of the board on the task")]
+        public Board? Board { get; set; }
+        [Required]
+        [Comment("The user who owe the task")]
+        public string OwnerId { get; set; } = string.Empty;
+        [ForeignKey(nameof(OwnerId))]
+        [Comment("Owner of the task")]
+        public IdentityUser Owner { get; set; } = new IdentityUser();
+    }
+}
+//⦁	Id – a unique integer, Primary Key
+//⦁	Title – a string with min length 5 and max length 70 (required)
+//⦁	Description – a string with min length 10 and max length 1000 (required)
+//⦁	CreatedOn – date and time
+//⦁	BoardId – an integer
+//⦁	Board – a Board object
+//⦁	OwnerId – an integer (required)
+//⦁	Owner – an IdentityUser object
