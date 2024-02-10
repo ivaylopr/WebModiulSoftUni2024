88fa5432881953fda8bff4fa5507db0dccfcee8c
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2024-02-10 11:50:26 +0200

    adding functions about the task
    creating tasks
    view task details

--- TaskBoardApp/Controllers/TaskController.cs
+++ TaskBoardApp/Controllers/TaskController.cs
@@ -13,44 +13,65 @@ namespace TaskBoardApp.Controllers
             data = _dbContext;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
         [HttpGet]
         public async Task<IActionResult> Create()
         {
             TaskFormModel taskModel = new TaskFormModel()
             {
-                Boards = GetBoards()
+                Boards = await GetBoards()
             };
             return View(taskModel);
         }
         [HttpPost]
         public async Task<IActionResult> Create(TaskFormModel taskModel)
         {
-            if (!GetBoards().Any(b=>b.Id==taskModel.BoardId))
+            if (!(await GetBoards()).Any(b => b.Id == taskModel.BoardId))
             {
-                ModelState.AddModelError(nameof(taskModel.BoardId),"Board doesn't exist!");
+                ModelState.AddModelError(nameof(taskModel.BoardId), "Board doesn't exist!");
             }
             string currenUserId = GetUserId();
             if (!ModelState.IsValid)
             {
-                taskModel.Boards = GetBoards();
+                taskModel.Boards = await GetBoards();
                 return View(taskModel);
             }
-            Task task = new Task()
+            var task = new Data.Models.Task()
             {
-                Title = taskModel.Title,
-                Description = taskModel.Description,
+                BoardId=taskModel.BoardId,
+                Description=taskModel.Description,
+                Title=taskModel.Title,
+                CreatedOn= DateTime.Now,
+                OwnerId = currenUserId,
             };
-            return View(taskModel);
+            await data.AddAsync(task);
+            await data.SaveChangesAsync();  
+            return RedirectToAction("Index","Board");
         }
 
-        private async Task<string> GetUserId()
-        => await User.FindFirstValueAsync(ClaimTypes.NameIdentifier);
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var task = data.Tasks.Where(t => t.Id == id)
+                .Select(t => new TaskDetailsViewModel()
+                {
+                    Id = t.Id,
+                    Description = t.Description,
+                    Title = t.Title,
+                    CreatedOn=t.CreatedOn.ToString(),
+                    Board = t.Board.Name,
+                    Owner = t.Owner.UserName
+                });
+            if (task==null)
+            {
+                return BadRequest();
+            }
+            return View(task);
+        }
 
-        private IEnumerable<TaskBoardModel> GetBoards()
+        private string GetUserId()
+        => User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        private async Task<IEnumerable<TaskBoardModel>> GetBoards()
         => data.Boards.Select(b => new TaskBoardModel()
         {
             Id = b.Id,
